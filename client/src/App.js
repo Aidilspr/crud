4276e9cc80f0fc1791de55e5c74a94f94c69ea10
Aidilspru <aidilsaputra05122022@gmail.com>
2023-07-12 06:55:05 +0700

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Main from "./components/Main";
+import AddUser from "./components/Main/AddUser";
+import EditUser from "./components/Main/EditUser";
+import UserList from "./components/Main/UserList";
+
 import Signup from "./components/Singup";
 import Login from "./components/Login";
 
@@ -8,8 +11,13 @@ function App() {
 
 	return (
 		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
+			{user && <Route path="/" exact element={<UserList />} />}
+
 			<Route path="/signup" exact element={<Signup />} />
+			<Route path="edit/:id" element={<EditUser />} />
+			<Route path="/add" element={<AddUser />} />
+
+
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
 		</Routes>
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const UsertRoute = require("./routes/UserRoute");
 
 // database connection
 connection();
@@ -12,10 +13,11 @@ connection();
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(UsertRoute);
 
 // routes
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 app.listen(port, console.log(`Listening on port ${port}...`));
